49e9f7e2000b5eec4c42fd5208f6187ae7d7de85
ИИП ТТ <kab31lab@gmail.com>
2023-03-16 12:30:57 +0200

    3 zad

--- Program.cs
+++ Program.cs
@@ -14,15 +14,24 @@ namespace ConsoleApp34
             int[] array = {8, -1, 4, 16, -5, 3};
             int max = array[0];
             int indexMax = 0;
-            for(int i = 1; i<array.Length; i++)
+            int min = array[0];
+            int indexMin = 0;
+
+            for (int i = 1; i < array.Length; i++)
             {
-                if (array[i]>max)
-                    {
+                if (array[i] > max)
+                {
                     max = array[i];
                     indexMax = i;
                 }
-            }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    indexMin = i;
+                }
+            }  
             Console.WriteLine($"max={max}\t indexMAX={indexMax}");
+            Console.WriteLine($"min={min}\t indexMIN={indexMin}");
             Console.Read();
         }
     }
