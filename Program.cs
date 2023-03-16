b0dfda54888f61af8f6ceef52431afb1687bd9d8
ИИП ТТ <kab31lab@gmail.com>
2023-03-16 11:53:12 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //нахождение максимального элемента в массиве
+            int[] array = {8, -1, 4, 16, -5, 3};
+            int max = array[0];
+            for(int i = 1; i<array.Length; i++)
+            {
+                if (array[i]>max)
+                    {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine($"max = {max}");
+            Console.Read();
+        }
+    }
+}
